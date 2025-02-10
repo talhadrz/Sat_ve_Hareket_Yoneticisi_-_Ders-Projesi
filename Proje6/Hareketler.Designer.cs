85355aa410879702f6a8694a0d2e50ab87b1f6f3
Talha_Durmaz <144232044+talhadrz@users.noreply.github.com>
2025-02-10 14:59:17 +0300

    Add project files.

---
+++ Proje6/Hareketler.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Proje6
+{
+    partial class Hareketler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbAd = new System.Windows.Forms.ComboBox();
+            this.cmbMusterı = new System.Windows.Forms.ComboBox();
+            this.cmbPersonel = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFıat = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Label();
+            this.btnGuncelle = new System.Windows.Forms.Label();
+            this.btnSıl = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1022, 367);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnSıl);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtFıat);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbPersonel);
+            this.groupBox1.Controls.Add(this.cmbMusterı);
+            this.groupBox1.Controls.Add(this.cmbAd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 372);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1019, 262);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yeni Veri Girişi";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(3, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1022, 367);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(97, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(107, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Müşter:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(91, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Personel:";
+            // 
+            // cmbAd
+            // 
+            this.cmbAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbAd.FormattingEnabled = true;
+            this.cmbAd.Location = new System.Drawing.Point(190, 73);
+            this.cmbAd.Name = "cmbAd";
+            this.cmbAd.Size = new System.Drawing.Size(210, 28);
+            this.cmbAd.TabIndex = 3;
+            // 
+            // cmbMusterı
+            // 
+            this.cmbMusterı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbMusterı.FormattingEnabled = true;
+            this.cmbMusterı.Location = new System.Drawing.Point(190, 107);
+            this.cmbMusterı.Name = "cmbMusterı";
+            this.cmbMusterı.Size = new System.Drawing.Size(210, 28);
+            this.cmbMusterı.TabIndex = 4;
+            // 
+            // cmbPersonel
+            // 
+            this.cmbPersonel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbPersonel.FormattingEnabled = true;
+            this.cmbPersonel.Location = new System.Drawing.Point(190, 141);
+            this.cmbPersonel.Name = "cmbPersonel";
+            this.cmbPersonel.Size = new System.Drawing.Size(210, 28);
+            this.cmbPersonel.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(124, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fiyat:";
+            // 
+            // txtFıat
+            // 
+            this.txtFıat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFıat.Location = new System.Drawing.Point(190, 175);
+            this.txtFıat.Name = "txtFıat";
+            this.txtFıat.Size = new System.Drawing.Size(210, 27);
+            this.txtFıat.TabIndex = 7;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.AutoSize = true;
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnEkle.Location = new System.Drawing.Point(514, 65);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(65, 29);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.AutoSize = true;
+            this.btnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnGuncelle.Location = new System.Drawing.Point(488, 171);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(117, 29);
+            this.btnGuncelle.TabIndex = 9;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSıl
+            // 
+            this.btnSıl.AutoSize = true;
+            this.btnSıl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSıl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSıl.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnSıl.Location = new System.Drawing.Point(524, 119);
+            this.btnSıl.Name = "btnSıl";
+            this.btnSıl.Size = new System.Drawing.Size(44, 29);
+            this.btnSıl.TabIndex = 10;
+            this.btnSıl.Text = "Sil";
+            this.btnSıl.Click += new System.EventHandler(this.btnSıl_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtId.Location = new System.Drawing.Point(190, 40);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(210, 27);
+            this.txtId.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Blue;
+            this.label8.Location = new System.Drawing.Point(78, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Hareket Id:";
+            // 
+            // Hareketler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1024, 633);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Hareketler";
+            this.Text = "Hareketler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbPersonel;
+        private System.Windows.Forms.ComboBox cmbMusterı;
+        private System.Windows.Forms.ComboBox cmbAd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label btnSıl;
+        private System.Windows.Forms.Label btnGuncelle;
+        private System.Windows.Forms.Label btnEkle;
+        private System.Windows.Forms.TextBox txtFıat;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Proje6/Hareketler.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Proje6
+{
+    public partial class Hareketler : Form
+    {
+        public Hareketler()
+        {
+            InitializeComponent();
+        }
+        SqlConnection bag = new SqlConnection(@"Data Source=LAPTOP-4ROLKKOD;Initial Catalog=Proje6_2;Integrated Security=True;");
+        SqlDataAdapter Data = new SqlDataAdapter();
+        SqlCommand Komut = new SqlCommand();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Komut.Connection = bag;
+            TabloDoldur();
+            cmbDoldur();
+            Komut.Parameters.Add(new SqlParameter("@Urun", SqlDbType.Int) { Value = 0 });
+            Komut.Parameters.Add(new SqlParameter("@Musterı", SqlDbType.Int) { Value = 0 });
+            Komut.Parameters.Add(new SqlParameter("@Personel", SqlDbType.Int) { Value = 0 });
+            Komut.Parameters.Add(new SqlParameter("@Fıyet", SqlDbType.Int) { Value = 0 });
+            Komut.Parameters.Add(new SqlParameter("@ID", SqlDbType.Int) { Value = 0 });
+        }
+
+        private void TabloDoldur()
+        {
+
+            Data = new SqlDataAdapter("Execute Proje6", bag);
+            DataTable dt = new DataTable();
+            Data.Fill(dt);
+            dataGridView1.DataSource = dt;
+            txtId.Text = null;
+            txtFıat.Text = null;
+        }
+        private void cmbDoldur()
+        {
+            Data = new SqlDataAdapter("select * from URUNLER", bag);
+            DataTable Tablo = new DataTable();
+            Data.Fill(Tablo);
+            cmbAd.DisplayMember = "AD";
+            cmbAd.ValueMember = "ID";
+            cmbAd.DataSource = Tablo;
+
+            Data = new SqlDataAdapter("select * from MUSTERILER", bag);
+            DataTable Tablo2 = new DataTable();
+            Data.Fill(Tablo2);
+            cmbMusterı.DisplayMember = "ADSOYAD";
+            cmbMusterı.ValueMember = "ID";
+            cmbMusterı.DataSource = Tablo2;
+
+            Data = new SqlDataAdapter("select * from PERSONELLER", bag);
+            DataTable Tablo3 = new DataTable();
+            Data.Fill(Tablo3);
+            cmbPersonel.DisplayMember = "AD";
+            cmbPersonel.ValueMember = "ID";
+            cmbPersonel.DataSource = Tablo3;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                int satır = dataGridView1.SelectedCells[0].RowIndex;
+
+                txtId.Text = dataGridView1.Rows[satır].Cells[0].Value.ToString();
+                cmbAd.Text = dataGridView1.Rows[satır].Cells[1].Value.ToString();
+                cmbMusterı.Text = dataGridView1.Rows[satır].Cells[2].Value.ToString();
+                cmbPersonel.Text = dataGridView1.Rows[satır].Cells[3].Value.ToString();
+                txtFıat.Text = dataGridView1.Rows[satır].Cells[4].Value.ToString();
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            bag.Open();
+            Komut.CommandText = "insert into HAREKETLER (URUN,MUSTERI,personel,FIYAT) values (@Urun,@Musterı,@Personel,@Fıyet)";
+            DegısıklıkKaydet();
+        }
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            bag.Open();
+            Komut.CommandText = "UPDATE HAREKETLER SET URUN=@Urun,MUSTERI=@Musterı,PERSONEL=@Personel,FIYAT=@Fıyet WHERE HAREKETID=@ID";
+            DegısıklıkKaydet();
+        }
+        private void btnSıl_Click(object sender, EventArgs e)
+        {
+
+            bag.Open();
+            Komut.CommandText = "delete HAREKETLER where HAREKETLER.HAREKETID = @ID";
+            DegısıklıkKaydet();
+        }
+        void DegısıklıkKaydet()
+        {
+            try
+            {
+                Komut.Parameters["@urun"].Value = cmbAd.SelectedValue.ToString();
+                Komut.Parameters["@Musterı"].Value = cmbMusterı.SelectedValue.ToString();
+                Komut.Parameters["@Personel"].Value = cmbPersonel.SelectedValue.ToString();
+                Komut.Parameters["@Fıyet"].Value = txtFıat.Text;
+                Komut.Parameters["@ID"].Value = txtId.Text;
+                Komut.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Veritabanı hatası: " + ex.Message, "SQL Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Bağlantı hatası: " + ex.Message, "Bağlantı Hatası", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Lütfen fiyat ve ID alanlarına geçerli bir sayı girin!", "Format Hatası", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Eksik bilgi: " + ex.Message, "Null Hatası", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bilinmeyen bir hata oluştu: " + ex.Message, "Genel Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (bag.State == ConnectionState.Open)
+                {
+                    bag.Close();
+                    TabloDoldur();
+                }
+            }
+        }
+    }
+}
